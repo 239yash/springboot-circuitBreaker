9bb7910c8b8a6480ea17acafd52ca815a5fc1979
239yash <239yash@gmail.com>
2023-11-15 00:01:26 +0530

    add: initial commit for project setup

---
+++ src/main/java/com/demo/springbootcircuitBreaker/SpringbootCircuitBreakerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springbootcircuitBreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCircuitBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCircuitBreakerApplication.class, args);
+	}
+
+}
