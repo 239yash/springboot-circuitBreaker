6c84ae28e9222cec0fd47e08d0e5d52450b7d655
239yash <239yash@gmail.com>
2023-11-15 18:02:31 +0530

    add: circuit breaker config setup

---
+++ src/main/java/com/demo/springbootcircuitBreaker/Config/CircuitBreakerConfigurationClass.java
@@ -0,0 +1,38 @@
+package com.demo.springbootcircuitBreaker.Config;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class CircuitBreakerConfigurationClass {
+    private static CircuitBreaker circuitBreaker;
+
+    private CircuitBreakerConfig createCircuitBreakerConfiguration() {
+        return CircuitBreakerConfig.custom()
+                .failureRateThreshold(5)
+                .automaticTransitionFromOpenToHalfOpenEnabled(true)
+                .waitDurationInOpenState(Duration.ofSeconds(10))
+                .permittedNumberOfCallsInHalfOpenState(3)
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                .slidingWindowSize(100)
+                .recordExceptions(Exception.class)
+                .build();
+    }
+
+    @Bean
+    private void createCircuitBreakerInstance() {
+        // Create a CircuitBreakerRegistry with a custom global configuration
+        CircuitBreakerRegistry circuitBreakerRegistry =
+                CircuitBreakerRegistry.of(createCircuitBreakerConfiguration());
+        circuitBreaker = circuitBreakerRegistry.circuitBreaker("cbDemo");
+    }
+
+    public CircuitBreaker getCircuitBreakerInstance() {
+        return circuitBreaker;
+    }
+}
