2205c3d859bcb22530819e83076f500426aa209a
239yash <239yash@gmail.com>
2023-11-16 22:59:21 +0530

    add: service & controller for circuit breaker demo

---
+++ src/main/java/com/demo/springbootcircuitBreaker/Controller/RestApiController.java
@@ -0,0 +1,20 @@
+package com.demo.springbootcircuitBreaker.Controller;
+
+import com.demo.springbootcircuitBreaker.Service.MainService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+@AllArgsConstructor
+public class RestApiController {
+    private final MainService mainService;
+    @GetMapping
+    public String getController() {
+        return "";
+    }
+}
