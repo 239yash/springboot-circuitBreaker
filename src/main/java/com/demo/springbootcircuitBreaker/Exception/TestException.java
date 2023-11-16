c224786c28a95e653087b069a9ff70999ac5ff0e
239yash <239yash@gmail.com>
2023-11-17 00:09:03 +0530

    add: custom exception class for cb record parameter

---
+++ src/main/java/com/demo/springbootcircuitBreaker/Exception/TestException.java
@@ -0,0 +1,8 @@
+package com.demo.springbootcircuitBreaker.Exception;
+
+public class TestException extends RuntimeException{
+
+    public TestException(String message) {
+        super(message);
+    }
+}
