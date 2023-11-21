15483bfa5bf798c4c86fc55711fd081329cf262a
239yash <239yash@gmail.com>
2023-11-21 08:53:50 +0530

    update: changes service for test exception logs

--- src/main/java/com/demo/springbootcircuitBreaker/Service/MainService.java
+++ src/main/java/com/demo/springbootcircuitBreaker/Service/MainService.java
@@ -2,6 +2,7 @@ package com.demo.springbootcircuitBreaker.Service;
 
 import com.demo.springbootcircuitBreaker.Config.CircuitBreakerConfigurationClass;
 import com.demo.springbootcircuitBreaker.Exception.TestException;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,26 +15,24 @@ import java.util.function.Supplier;
 @AllArgsConstructor
 public class MainService {
     private final CircuitBreakerConfigurationClass circuitBreakerConfigurationClass;
-    private static Integer counter = 1;
+    private static Integer counter = 0;
     public String testMethod() {
+        counter++;
         CircuitBreaker circuitBreaker = circuitBreakerConfigurationClass.getCircuitBreakerInstance();
         log.info("Counter - " + counter + " Circuit Breaker State - " + circuitBreaker.getState());
         Supplier<String> decoratedSupplier = CircuitBreaker.decorateSupplier(circuitBreaker, this::thirdPartyServiceCall);
         try {
             return "Counter - " + counter + " " + decoratedSupplier.get();
-        } catch (Exception e) {
+        } catch (TestException e) {
             // Handle exception or log
             return "Fallback response: " + e.getMessage();
-        } finally {
-            counter++;
+        } catch (CallNotPermittedException ex) {
+            return "CallNotPermitted response: " + ex.getMessage();
         }
     }
 
 
     private String thirdPartyServiceCall() {
-        if (Math.random() < 0.7) {
-            throw new TestException("Remote service failed");
-        }
-        return "Remote call successful";
+        throw new TestException("Remote service failed");
     }
 }
